14d299bd6e79e107aeaf2d9f602ebdbc5c7c1d80
viv500 <vivek.j.manoj@gmail.com>
2024-07-23 13:37:24 -0400

    Prints course name, anti-reqs, pre-reqs. Need to fix anti-reqs and pre-reqs showing the same content. The div tags' classes are identical for both on the webpage

--- selenium-scrape.py
+++ selenium-scrape.py
@@ -1,73 +1,76 @@
-#imports
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
 options = Options()
-
-# this prevents the window from closing after operating
 options.add_experimental_option("detach", True)
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-# explore page url (where the list of courses are)
 url = "https://uwflow.com/explore"
-
-#takes the driver to the home page
 driver.get(url)
 
-# this is to allow the page to load. it gives it up to 50 seconds
 wait = WebDriverWait(driver, 50)
 links = set()
 
-# to find the height of the current webpages body
 last_height = driver.execute_script("return document.body.scrollHeight")
 
 while True:
-    # this captures all anchor tags
     elements = driver.find_elements(By.XPATH, "//a[@href]")
     for element in elements:
-
-        # to extract relevant data from the anchor tag. i.e. the actual url to each courses page
-        # for example, https://uwflow.com/course/mte262
         links.add(element.get_attribute('href'))
 
-    # scrolling down (using the current webpage height we calculated earlier)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-
-    # lmao waits 1 second to make sure stuff loads properly
     time.sleep(1)
 
-    # to see if the new content was loaded
     new_height = driver.execute_script("return document.body.scrollHeight")
     if new_height == last_height:
-        break  # exit the loop if no new content
+        break
 
     last_height = new_height
 
 print(f"Total unique links found: {len(links)}")
 
+course_data_list = []
+
 for link in links:
-    print(link) #prints the url to each course page
+    driver.get(link)
 
-    driver.get(link) #this line directs u to each course page from the links list
+    try:
+        title_class = 'sc-pktCe eHAbVk'
+        print(f"Processing link: {link}")
+        
+        #xpaths
+        prereq_xpath = "//div[@class='sc-ptdsS hxDuVK']"
+        coreq_xpath = "//div[@class='sc-pkIrX fqDIne']"
+        antireq_xpath = "//div[@class='sc-ptdsS hxDuVK']"
 
-    # Now that are u are in each page, scrape data as needed. u might wanna use beautifulsoup4 here
+        #waiting for those elements to load
+        prereq_element = wait.until(EC.presence_of_element_located((By.XPATH, prereq_xpath)))
+        coreq_element = wait.until(EC.presence_of_element_located((By.XPATH, coreq_xpath)))
+        antireq_element = wait.until(EC.presence_of_element_located((By.XPATH, antireq_xpath)))
 
-    #*************************************************************************************************
+        #print(f"Link: {link}")
+
+        # extracting course name from url
+        print(f"Course: {link[26:]}")
+        print(f"Prerequisites: {prereq_element.text}")
+        print(f"Corequisites: {coreq_element.text}")
+        print(f"Antirequisites: {antireq_element.text}")
+        print(" ")
 
 
+    except Exception as e:
+        print(f"An error occurred while scraping {link}: {e}")
 
-
-
-    #*************************************************************************************************
-
-    time.sleep(1)  # some more time
-
+    time.sleep(1)
 
 # Optional: close the driver
 # driver.quit()
+
+print(f"Total courses scraped: {len(course_data_list)}")
